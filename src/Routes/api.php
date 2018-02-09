967dd777355c37f431fbf3b402e10ab6f0f0a170
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-02-09 15:24:33 +1000

    Scope/namespace fix

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -2,6 +2,6 @@
 
 $controller = 'Pivotal\ApiAuthentication\Http\Api\Controllers\AuthenticationController';
 
-Route::group(['prefix' => 'api'], function () {
+Route::group(['prefix' => 'api'], function () use ($controller) {
     Route::post('/login', $controller . '@authenticate');
 });
