8121f6b71c2e1bb8f743037769a09de091db479a
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-02-14 09:59:58 +1000

    Let's make a jsonapi.org compliant response for authentication

--- src/Http/Controllers/AuthenticationController.php
+++ src/Http/Controllers/AuthenticationController.php
@@ -5,26 +5,52 @@ namespace Pivotal\ApiAuthentication\Http\Controllers;
 use JWTAuth;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthenticateController extends Controller
 {
+    protected $response;
+
+    public function __construct()
+    {
+        $this->response = new \stdClass();
+    }
+
     public function authenticate(Request $request)
     {
-        // grab credentials from the request
         $credentials = $request->only('email', 'password');
 
-        try {
-            // attempt to verify the credentials and create a token for the user
-            if (!$token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'invalid_credentials'], 401);
-            }
-        } catch (JWTException $e) {
-            // something went wrong whilst attempting to encode the token
-            return response()->json(['error' => 'could_not_create_token'], 500);
+        $validator = Validator::make($credentials, [
+            'email' => 'bail|required',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $this->response->status = 401;
+            $this->response->errors['errors'][] = ['message' => 'You must supply credentials'];
+
+            return response()->json($this->response->errors, $this->response->status);
         }
 
-        // all good so return the token
-        return response()->json(compact('token'));
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                $this->response->status = 401;
+                $this->response->errors['errors'][] = ['message' => 'Invalid credentials supplied'];
+
+                return response()->json($this->response->errors, $this->response->status);
+            }
+        } catch (JWTException $e) {
+            $this->response->status = 500;
+            $this->response->errors['errors'][] = ['message' => 'Unknown error, could not create token'];
+
+            return response()->json($this->response->errors, $this->response->status);
+        }
+
+
+        $this->response->status = 200;
+        $this->response->data['data'][] = compact('token');
+
+        return response()->json($this->response->data, $this->response->status);
     }
 }
